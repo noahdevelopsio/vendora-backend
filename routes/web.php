37103f271b6393a7d651ba855f1ac7ee32aac74e
noahbig001 <noahbig001@gmail.com>
2025-11-21 22:56:25 +0000

    fix: Resolve view not found error and database issues

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'Welcome to the Vendora API']);
 });
 
 Route::get('/product/{product}', [ProductController::class, 'show'])->name('product.show');
